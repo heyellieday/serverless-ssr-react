c70fd3403b283526555469c04d6ca0b918f8fdda
Ellie Day <heyellieday@gmail.com>
2018-04-01 00:49:31 -0700

    Promisify uploadToS3 function to allow passing of file hash to serverless yml

--- deploy.js
+++ deploy.js
@@ -2,5 +2,4 @@ const shell = require("shelljs");
 
 const options = process.argv.slice(2, 4).join(" ");
 
-const hash = "hi"; //shell.exec("node ./utils/uploadToS3.js", { silent: true }).stdout;
-shell.exec(`serverless deploy -v ${options} --bundleHash ${hash}`);
+shell.exec(`serverless deploy -v ${options}`);
--- utils/uploadToS3.js
+++ utils/uploadToS3.js
@@ -2,27 +2,34 @@ import fs from "fs";
 import aws from "aws-sdk";
 import fileToHash from "./fileToHash";
 
-const s3 = new aws.S3();
-const BUNDLE_LOCATION = "dist/bundle.js";
+module.exports.promised = () => {
+  const promise = new Promise((resolve, reject) => {
+    const s3 = new aws.S3();
+    const BUNDLE_LOCATION = "dist/bundle.js";
 
-// Bucket names must be unique across all S3 users
+    // Bucket names must be unique across all S3 users
 
-var myBucket = process.env.ASSETS_BUCKET;
-console.log("test");
-fs.readFile(BUNDLE_LOCATION, function(err, data) {
-  if (err) {
-    throw err;
-  }
+    var myBucket = process.env.ASSETS_BUCKET;
 
-  const readStream = fs.createReadStream(BUNDLE_LOCATION);
-  fileToHash(readStream).then(bundleHash => {
-    params = {
-      Bucket: myBucket,
-      Key: `bundle-${bundleHash}.js`,
-      Body: data,
-      ACL: "public-read"
-    };
-    console.log(bundleHash);
-    s3.putObject(params, function(err, data) {});
+    fs.readFile(BUNDLE_LOCATION, function(err, data) {
+      if (err) {
+        throw err;
+      }
+
+      const readStream = fs.createReadStream(BUNDLE_LOCATION);
+      fileToHash(readStream).then(bundleHash => {
+        const bundleName = `bundle-${bundleHash}.js`;
+        params = {
+          Bucket: myBucket,
+          Key: bundleName,
+          Body: data,
+          ACL: "public-read"
+        };
+        s3.putObject(params, function(err, data) {
+          resolve(bundleName);
+        });
+      });
+    });
   });
-});
+  return promise;
+};
