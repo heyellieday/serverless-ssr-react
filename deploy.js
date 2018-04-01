88336f57de16d953924b29d5b8b0f86861b2fa24
Ellie Day <heyellieday@gmail.com>
2018-04-01 00:54:22 -0700

    use promise in deploy.js, passing bundle name to serverless

--- deploy.js
+++ deploy.js
@@ -1,5 +1,7 @@
 const shell = require("shelljs");
-
+const uploadToS3 = require("./utils/uploadToS3");
 const options = process.argv.slice(2, 4).join(" ");
 
-shell.exec(`serverless deploy -v ${options}`);
+uploadToS3.promised().then(bundleName => {
+  shell.exec(`serverless deploy -v ${options} --bundleName ${bundleName}`);
+});
